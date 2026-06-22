554ca3885e406460cf6d9351e4c68dc0471d2b06
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-23 00:10:47 +0700

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -14,13 +14,10 @@ pluginManagement {
     }
 }
 
-// Definisikan ulang di sini
-val kotlinVersion = "2.0.0"
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
 
 include(":app")
