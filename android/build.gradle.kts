77ec4c8640ce04ecc973f6fe26c6f2fe20671c1f
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-23 14:24:45 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,3 @@
-buildscript {
-    val kotlin_version = "2.0.0"
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-    }
-}
-
 allprojects {
     repositories {
         google()
